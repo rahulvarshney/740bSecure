44e5a7fef4dd899f0465d17da218bc501c241d9a
rahulvarshney <rahulvarshney@gmail.com>
2025-08-28 15:06:24 -0700

    0.0.12

--- routes/index.tsx
+++ routes/index.tsx
@@ -58,13 +58,16 @@ export const handler: Handlers<Props> = {
   },
 };
 
-  let agentPromise = readCsvFile("./database/data.csv");
+  const agentPromise = readCsvFile("./database/data.csv");
   let agentName = null;
+  let agent = null;
   agentPromise.then(data => {
     console.log('inside index.tsx');
     console.log(data[0].Name); // Output: Data fetched successfully! (after 2 seconds)
     agentName = data[0].Name;
+    agent = data;
     console.log('agentName is: ' + agentName);
+    console.log('agent[0] is: ' + agent[0].Name);
   })
 
 export default function Upload(props: PageProps<Props>) {
@@ -78,6 +81,9 @@ export default function Upload(props: PageProps<Props>) {
             <p className="text-sm opacity-90">
               agentName should show here:
            { agentName }    
+            </p>
+            <p>
+              { agent[0].Name } - { agent[0].License }
             </p>
           </div>
         </div>
@@ -104,7 +110,7 @@ export default function Upload(props: PageProps<Props>) {
 
           <div className="bg-purple-500 text-white p-4 rounded-md text-center">
             <h2 className="text-lg font-semibold">Element 3</h2>
-            <p className="text-sm opacity-90">0.0.11 © 2025 Varshney & Son</p>
+            <p className="text-sm opacity-90">0.0.12 © 2025 Varshney & Son</p>
           </div>
         </div>
 
