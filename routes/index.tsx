581f80ec07dba8a526a70410135f574d611ea47b
rahulvarshney <rahulvarshney@gmail.com>
2025-08-29 16:19:13 -0700

    0.0.22

--- routes/index.tsx
+++ routes/index.tsx
@@ -79,7 +79,7 @@ export default function Upload(props: PageProps<Props>) {
   return (
     <>
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+  viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2"
   stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
   <path
     d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
@@ -176,7 +176,7 @@ export default function Upload(props: PageProps<Props>) {
 
           <div className="bg-purple-500 text-white p-4 rounded-md text-center">
             <h2 className="text-lg font-semibold">Element 3</h2>
-            <p className="text-sm opacity-90">0.0.21 © 2025 Varshney & Son</p>
+            <p className="text-sm opacity-90">0.0.22 © 2025 Varshney & Son</p>
           </div>
         </div>
 
