eb4beae7b7731ed65eb09e25634af8c339e2445e
rahulvarshney <rahulvarshney@gmail.com>
2025-08-30 11:43:22 -0700

    0.0.29

--- routes/index.tsx
+++ routes/index.tsx
@@ -105,17 +105,31 @@ export default function Upload(props: PageProps<Props>) {
       {/* Contact Information */}
       <div className="space-y-3 mb-4">
         <div className="flex items-center gap-3">
-          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#6082B6"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-phone-with-shadow"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  
+          fill="none"  stroke="#6082B6"  stroke-width="2"  stroke-linecap="round"  
+          stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-phone">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+          </svg>
           <span className="text-sm text-gray-700"> { agent[0].Cell } </span>
         </div>
 
         <div className="flex items-center gap-3">
-
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  
+          fill="none"  stroke="#6082B6"  stroke-width="2"  stroke-linecap="round"  
+          stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-mail">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" />
+          </svg>
           <span className="text-sm text-gray-700">ash@loansbyash.com</span>
         </div>
 
         <div className="flex items-center gap-3">
-
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  
+          fill="none"  stroke="#6082B6"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-world-www"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 7a9 9 0 0 0 -7.5 -4a8.991 8.991 0 0 0 -7.484 4" /><path d="M11.5 3a16.989 16.989 0 0 0 -1.826 4" /><path d="M12.5 3a16.989 16.989 0 0 1 1.828 4" /><path d="M19.5 17a9 9 0 0 1 -7.5 4a8.991 8.991 0 0 1 -7.484 -4" /><path d="M11.5 21a16.989 16.989 0 0 1 -1.826 -4" /><path d="M12.5 21a16.989 16.989 0 0 0 1.828 -4" />
+          <path d="M2 10l1 4l1.5 -4l1.5 4l1 -4" /><path d="M17 10l1 4l1.5 -4l1.5 4l1 -4" />
+          <path d="M9.5 10l1 4l1.5 -4l1.5 4l1 -4" />
+          </svg>
           <span className="text-sm text-gray-700">www.loansbyash.com</span>
         </div>
       </div>
@@ -180,7 +194,7 @@ export default function Upload(props: PageProps<Props>) {
 
           <div className="bg-purple-500 text-white p-4 rounded-md text-center">
             <h2 className="text-lg font-semibold">Element 3</h2>
-            <p className="text-sm opacity-90">0.0.27 © 2025 Varshney & Son</p>
+            <p className="text-sm opacity-90">0.0.29 © 2025 Varshney & Son</p>
           </div>
         </div>
 
