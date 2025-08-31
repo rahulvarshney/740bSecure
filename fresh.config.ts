93b5b170260f0e5fb510cbd0d1731f9bc46c666e
rahulvarshney <rahulvarshney@gmail.com>
2025-08-31 13:21:30 -0700

    0.0.38

--- fresh.config.ts
+++ fresh.config.ts
@@ -5,4 +5,7 @@ export default defineConfig({
   plugins: [
     tailwind()
   ],
+  server: {
+    port: 3000,
+  }
 });
--- routes/index.tsx
+++ routes/index.tsx
@@ -212,7 +212,7 @@ export default function Upload(props: PageProps<Props>) {
 
           <div className="bg-purple-500 text-white p-4 rounded-md text-center">
             <h2 className="text-lg font-semibold">Element 3</h2>
-            <p className="text-sm opacity-90">0.0.37 © 2025 Varshney & Son</p>
+            <p className="text-sm opacity-90">0.0.38 © 2025 Varshney  & Son</p>
           </div>
         </div>
 
